3b3a25ce8dddebaa87b498a2a0348e8ee7acb058
Mazen <mazennnnnnnnn@gmail.com>
2017-08-17 15:41:41 +0200

    Added SPOJ ALIEN

---
+++ SPOJ/ALIEN.java
@@ -0,0 +1,74 @@
+package SPOJ;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ALIEN {
+
+	public static void main(String[] args) throws IOException {
+		MyScanner sc = new MyScanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int tc = sc.nextInt();
+		while (tc-- > 0) {
+			int n = sc.nextInt(), s = sc.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = sc.nextInt();
+			int ans = 0, len = 0;
+			for (int i = 0, j = 0, sum = 0; i < n; i++) {
+				while (j < n && sum + a[j] <= s)
+					sum += a[j++];
+				if (sum <= s && len < j - i) {
+					len = j - i;
+					ans = sum;
+				}
+				sum -= a[i];
+			}
+			out.printf("%d %d\n", ans, len);
+		}
+		out.flush();
+		out.close();
+	}
+
+	static class MyScanner {
+
+		BufferedReader br;
+		StringTokenizer st;
+
+		MyScanner(InputStream is) {
+			br = new BufferedReader(new InputStreamReader(is));
+		}
+
+		MyScanner(String filename) throws IOException {
+			br = new BufferedReader(new FileReader(new File(filename)));
+		}
+
+		String next() throws IOException {
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+
+		String nextLine() throws IOException {
+			return br.readLine();
+		}
+
+		int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
+
+		boolean ready() throws IOException {
+			return br.ready();
+		}
+	}
+}
